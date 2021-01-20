82a12b0d483d85d9c54347ba44db91f25b83d2be
ohad <ohad@ohads-Mac-mini.local>
2021-01-20 16:46:06 +0200

    improved service

--- setup.sh
+++ setup.sh
@@ -3,6 +3,7 @@
 bold='\033[1m'
 red='\e[31m'
 green='\e[32m'
+light_green='\e[92m'
 yel='\e[33m'
 blu='\e[34m'
 mag='\e[35m'
@@ -14,29 +15,34 @@ checkmark="${green}\u2714${end}"
 
 
 print_pink(){
-    msg=$1
-    echo -e "${bold}${cyn}${msg}${end}"
+  msg=$1
+  echo -e "${bold}${cyn}${msg}${end}"
+}
+
+print_bold(){
+  msg=$1
+  echo -e "${bold}${msg}${end}"
 }
 
 print_info(){
-    	msg=$1
-    	echo -e "${bold}${msg}${end}"
+  msg=$1
+  echo -e "${green}${msg}${end}"
 }
 
 failure(){
 	msg=$1
-    	echo -e "${crossmark} ${red}${msg}${end}"
-    	exit -1
+  echo -e "${crossmark} ${red}${msg}${end}"
+  exit -1
 }
 
 success(){
-    	msg=$1
-    	echo -e "${checkmark} ${green}${msg}${end}"
+  msg=$1
+  echo -e "${checkmark} ${light_green}${msg}${end}"
 }
 
 success_bold(){
-    	msg=$1
-    	echo -e "${bold}${green}${msg}${end}"
+  msg=$1
+  echo -e "${bold}${green}${msg}${end}"
 }
 
 check_exit_code(){
@@ -51,7 +57,7 @@ check_exit_code(){
 }
 
 check_root(){
-   	if [[ $EUID -ne 0 ]]; then
+  if [[ $EUID -ne 0 ]]; then
       		failure "This script must be run as root"
 	fi
 }
@@ -65,10 +71,10 @@ check_linux(){
 
 host_info(){
   host_info=$(hostnamectl)
-  print_info "-------------------    Host Info    ------------------------"
+  print_bold "-------------------    Host Info    ------------------------"
   print_pink "$host_info"
-  print_info "------------------------------------------------------------"
-  print_info "Starting Installation..."
+  print_bold "------------------------------------------------------------"
+  print_bold "Starting Installation..."
 }
 
 check_connection(){
@@ -87,32 +93,44 @@ install_yum_packages(){
 
 install_python3(){
   print_info "Installing Python3.8"
+  print_bold "- Downloading python"
   mkdir downloads
-  wget https://www.python.org/ftp/python/3.8.7/Python-3.8.7.tgz -P downloads
-  tar xvf downloads/Python-3.8.7.tgz -C downloads
+  wget https://www.python.org/ftp/python/3.8.7/Python-3.8.7.tgz -P downloads > /dev/null 2>&1
+  tar xvf downloads/Python-3.8.7.tgz -C downloads > /dev/null
   pushd downloads/Python-3.8.7 > /dev/null
-  ./configure --enable-optimizations
-  make altinstall
+  print_bold "- Configure + Make"
+  ./configure --enable-optimizations > /dev/null 2>&1
+  make altinstall > /dev/null 2>&1
   popd > /dev/null
   ln -s /usr/local/bin/python3.8 /usr/local/bin/python3
   ln -s /usr/local/bin/pip3.8 /usr/local/bin/pip3
   sed -i -e '/secure_path/ s[=.*[&:/usr/local/bin[' /etc/sudoers
-  ## ERROR HERE. doesn't reload changes in sudoers
-  python3 --version
+  export PATH="$PATH:/usr/local/bin"
+  python3 --version > /dev/null
   check_exit_code "python3 failed to install" "python3 installed successfully"
-  pip3 --version
+  pip3 --version > /dev/null
   check_exit_code "pip3 failed to install" "pip3 installed successfully"
-  pip3 install virtualenv
+  pip3 install virtualenv > /dev/null 2>&1
   check_exit_code "virtualenv failed to install" "virtualenv installed successfully"
 }
 
 create_virtualenv(){
+  print_info "Creating virtualenv"
   python3 -m virtualenv venv
   . venv/bin/activate
-  pip3 install -r requirements.txt
+  check_exit_code "virtualenv failed to create" "virtualenv created successfully"
+  print_info "Installing dependencies"
+  print_bold "$(cat requirements.txt)"
+  pip3 install -r requirements.txt > /dev/null
+  check_exit_code "virtualenv failed to install" "virtualenv created successfully"
+  deactivate
 }
 
-
+start_service(){
+  print_info "starting Bidera Service"
+  cp bidera.service /etc/systemd/system/
+  systemctl start bidera
+}
 
 main(){
   check_root
@@ -122,11 +140,9 @@ main(){
   install_yum_packages
   install_python3
   create_virtualenv
-
-  cp bidera.service /etc/systemd/system/
-  print_info "starting Bidera Service"
-  systemctl start bidera
+  start_service
 }
+
 if [[ "${BASH_SOURCE[0]}" == "${0}" ]]; then
     main "$@"
 fi
